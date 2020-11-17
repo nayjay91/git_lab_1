5f7cee137d671d45bf246ae9746b9068918ecce1
unknown <nayjay91@gmail.com>
2020-11-16 19:51:58 -0500

    Adding Regular expression for phone validation

--- clean_phone.py
+++ clean_phone.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
 def validate_phone(phone_number):
-    return True
+    """
+    Tests wheter phone numbers are valid
 
-print(validate_phone(123))
+    Arguements:
+    Phone number - A Pandas Series of Phone numbers as a string object
+
+    Returns:
+    A boolean Pandas Series
+    """
+    bool_phone = phone_number.str.contains(r"^\d{3}[-]?\d{3}[-]?\d{4}")
+    return bool_phone
+
+numbers = pd.Series(['156-152-1111','456-852-4562','(234)2346323'])
+print(validate_phone(numbers))
